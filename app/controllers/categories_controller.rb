0360bab2fd5674520fe027d1d33801b55efa4d41
Andy <andreapauline.deguzman@gmail.com>
2021-08-28 23:58:00 +0800

    controller test for category model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,44 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+      if @category.save
+        redirect_to categories_path
+      else
+        render :new
+    end
+  end
+
+
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+      if @category.update(category_params)
+        redirect_to categories_path
+      else
+        render :edit
+    end
+  end
+
+
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name, :category_body)
+  end
+
 end
